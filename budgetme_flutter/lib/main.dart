05bb3d57ef4aa6a641a324a407fd109fd2e153ec
Jake Alvarado <alvarado.jake22@gmail.com>
2023-10-26 16:36:29 -0700

    Code Translated to Flutter
    Had to redraft code due to hardware complications of using swift. Uploading new BudgetMe app using Flutter / Firebase instead of Swift / Atlas.

---
+++ budgetme_flutter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:budgetme_flutter/screens/signin_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Budget Me',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const SignInScreen());
+  }
+}
---
+++ budgetme_flutter/lib/screens/signin_screen.dart
@@ -0,0 +1,92 @@
+import 'package:budgetme_flutter/screens/home_screen.dart';
+import 'package:budgetme_flutter/screens/signup_screen.dart';
+import 'package:budgetme_flutter/widgets/reusable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.green,
+            Colors.blue,
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/budget-logo.png"),
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter Email", Icons.person_outline, false,
+                    _emailTextController),
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter Password", Icons.lock, true,
+                    _passwordTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                signInUpButton(context, true, () {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: _emailTextController.text,
+                          password: _passwordTextController.text)
+                      .then((value) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                    print("Signed In");
+                  }).onError((error, stackTrace) {
+                    //Show user some error
+                  });
+                }),
+                signUpOption()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account?",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            "  Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
