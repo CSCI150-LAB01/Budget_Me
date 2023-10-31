67dec0ddc9257dc1e3dc5c4eedb599fbc0030d38
RamonC3 <ramoncortes@mail.fresnostate.edu>
2023-10-31 12:08:29 -0700

    Changed buttons to match others

--- budgetme_flutter/lib/screens/home_screen.dart
+++ budgetme_flutter/lib/screens/home_screen.dart
@@ -46,23 +46,44 @@ class _HomeScreenState extends State<HomeScreen> {
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(5.0) //Sets how square outline of budget is
                     )
-                  
                   ),
               ),
+              SizedBox(height: 400,),
               Align(
               alignment: Alignment.bottomCenter,
               child: Container(
-                margin: const EdgeInsets.all(5),
-                width: double.infinity,
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
                 child: ElevatedButton(   
                     onPressed: () {
                         Navigator.push(context,
                           MaterialPageRoute(builder: (context) => UpdateBudgetScreen())
                         );//push
                     }, //onPressed
-                  child: const Text('Update Budget'),
+                  //---------------------- Button Style ----------------------------
+                  child: const Text('Update Budget',
+                  style: const TextStyle(color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
                   ),
-                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.black26;
+                        }
+                        return Colors.white;
+                      },
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                ),
+                    //------------------------------------------------------------
+                ),
+                ),
               ),
               
             ],
