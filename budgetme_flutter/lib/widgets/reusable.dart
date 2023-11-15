c60435de5d687f6ba36b9d1de18872977a8a3766
RamonC3 <ramoncortes@mail.fresnostate.edu>
2023-11-14 17:08:56 -0800

    new reusable update button

--- budgetme_flutter/lib/widgets/reusable.dart
+++ budgetme_flutter/lib/widgets/reusable.dart
@@ -99,6 +99,39 @@ Container confirmButton(BuildContext context, Function onTap){
     );
 }//confirmButton
 
+Container updateButton(BuildContext context, Function onTap){
+  return Container(
+    width: 50,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: (){
+        onTap();
+      },//onPressed
+      child: Text(
+        '+',
+        style: const TextStyle(
+            color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        style: ButtonStyle(
+           backgroundColor: MaterialStateProperty.resolveWith(
+          (states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.black26;
+            }
+            return Colors.white;
+          },
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        ),
+      ),
+    );
+}//confirmButton
 
 Container signInUpButton(BuildContext context, bool isLogin, Function onTap) {
   return Container(
