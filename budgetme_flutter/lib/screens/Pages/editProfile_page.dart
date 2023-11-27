dd849e5127c81c8bdafe78cd9cbd3b7161930cd1
RamonC3 <ramoncortes@mail.fresnostate.edu>
2023-11-27 15:54:26 -0800

    Begining of edit profile option

---
+++ budgetme_flutter/lib/screens/Pages/editProfile_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:budgetme_flutter/widgets/reusable.dart';
+
+class EditProfilePage extends StatefulWidget{
+@override
+_EditProfilePageState createState() => _EditProfilePageState();
+
+}
+class _EditProfilePageState extends State<EditProfilePage>{
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
